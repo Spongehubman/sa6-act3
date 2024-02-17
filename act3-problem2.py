0de97212e6f1f9d889ace2a8e74b8444e3d07a52
Nicholas Folis <heffawoozle77@gmail.com>
2024-02-17 01:00:48 -0600

    Added help citation from the aforementioned 'Python Lambda' W3schools.com webpage for 'act3-problem2.py'.

--- act3-problem2.py
+++ act3-problem2.py
@@ -4,8 +4,15 @@ string_list = ['cat', 'grasshopper', 'bash', 'stash', 'aardvark',
                 'cs3', 'archer', 'ton', 'goose', 'shell',
                 'arctic', 'penguin']
 
-
+# Reinforcement for lambda programming coding help came from the W3Schools.com webpage
+# titled "Python Lambda" (the source is below.)
 string_list.sort(key=lambda x: x)
 string_list.sort(key=lambda x: len(x))
 
 print(string_list)
+
+'''
+Source: "Python Lambda", W3Schools.com
+
+https://www.w3schools.com/python/python_lambda.asp
+'''
