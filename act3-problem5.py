a16cce1bc8f4c94a14c9c86be8965315e531f669
Nicholas Folis <heffawoozle77@gmail.com>
2024-02-16 21:31:37 -0600

    Modified and experimented with 'act3-problem5.py'.

--- act3-problem5.py
+++ act3-problem5.py
@@ -3,9 +3,9 @@ numbers = [1, 2, 3, 4, 5]
 
 # Some help for this code was taken from the W3Schools.com webpage
 # titled "Python Lambda" (the source is below.)
-power_list = list(map(lambda x, n: x ** n, numbers))
+power_list = list(map(lambda n: x ** n, numbers))
 
-print(power_list(numbers, 2))
+print(power_list(2))
 
 # For instance, if n = 2, then the output will be [1, 4, 9, 16, 25]
 
