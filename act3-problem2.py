e7ebfc05367ddc61ed8a46729d69e196c72e1d68
Nicholas Folis <heffawoozle77@gmail.com>
2024-02-16 21:23:32 -0600

    Completed 'act3-problem2.py', and added a citation for the W3Schools.com webpage, titled 'Python Lambda'.

--- act3-problem2.py
+++ act3-problem2.py
@@ -1,5 +1,9 @@
 
 
-power_list
+string_list = ['cat', 'grasshopper', 'bash', 'stash', 'aardvark', 'cs3',
+                'archer', 'ton', 'goose', 'shell']
 
 
+custom_sort = ()
+
+print(custom_sort)
---
+++ act3-problem3.py
@@ -0,0 +1,30 @@
+
+
+def find_maximum(l, lam):
+    
+    max = -1
+
+    # Lets assume the length of the list l is > 1. Then...
+
+    for i in range(len(l) - 1):
+        greater = lam(l[i], l[(i + 1)])
+
+        if greater > max:
+            max = greater
+
+    return max
+
+numbers = [3, 8, 2, 5, 1, 9, 4, 6, 7]
+
+# Some help for this code was taken from the W3Schools.com webpage
+# titled "Python Lambda" (the source is below.)
+compare = lambda x, y: x if x > y else y
+
+print(find_maximum(numbers, compare))
+
+
+'''
+Source: "Python Lambda", W3Schools.com
+
+https://www.w3schools.com/python/python_lambda.asp
+'''
