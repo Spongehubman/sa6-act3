16a5511e7563b2db988c83cd8c36c81fd232ae6b
Nicholas Folis <heffawoozle77@gmail.com>
2024-02-16 20:40:24 -0600

    Completed 'act3-problem4.py' and modified 'act3-problem5.py'. In both files, a citation was added for the W3Schools webpage titled 'Python Lambda'.

--- act3-problem4.py
+++ act3-problem4.py
@@ -1,8 +1,19 @@
 
 
-my_list1 = [1, 2, 3, 5, 6, 8, 9,]
-my_list2 = [0, 3, 4, 5, 7, 8, 9]
+my_list_combined = [[1, 2, 3, 5, 6, 8, 9,], [0, 3, 4, 5, 7, 8, 9]]
 
-list_intersection = list(filter(lambda x, y: x == y, my_list1, my_list2))
+my_list_1 = [1, 2, 3, 5, 6, 8, 9]
+my_list_2 = [0, 3, 4, 5, 7, 8, 9]
+
+
+# Some help for this code was taken from the W3Schools.com webpage
+# titled "Python Lambda" (the source is below.)
+list_intersection = list(filter(lambda x: x in my_list_2, my_list_1))
 
 print(list_intersection)
+
+'''
+Source: "Python Lambda", W3Schools.com
+
+https://www.w3schools.com/python/python_lambda.asp
+'''
--- act3-problem5.py
+++ act3-problem5.py
@@ -1,8 +1,17 @@
 
 numbers = [1, 2, 3, 4, 5]
 
-power_list = list(map(lambda x, n: x**n, numbers))
+# Some help for this code was taken from the W3Schools.com webpage
+# titled "Python Lambda" (the source is below.)
+power_list = list(map(lambda x, n: x ** n, numbers))
 
-print(power_list(n))
+print(power_list(numbers, 2))
 
 # For instance, if n = 2, then the output will be [1, 4, 9, 16, 25]
+
+
+'''
+Source: "Python Lambda", W3Schools.com
+
+https://www.w3schools.com/python/python_lambda.asp
+'''
